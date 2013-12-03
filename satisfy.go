ae957653a47e6a3be81c2c39f4bc1c56565fe8a5
NovemberFoxtrot <november@fox.trot>
2013-12-02 23:53:08 -0800

    zwei

--- satisfy.go
+++ satisfy.go
@@ -4,36 +4,28 @@ import (
 	"net/http"
 )
 
-type SingleHost struct {
-	handler http.Handler
-	allowedHost string
-}
+func SingleHost(handler http.Handler, allowedHost string) http.Handler {
+	ourFunc := func(w http.ResponseWriter, r *http.Request) {
+		host := r.Host
 
-func NewSingleHost(handler http.Handler, allowedHost string) *SingleHost {
-	return &SingleHost{handler: handler, allowedHost: allowedHost}
-}
-
-func (s *SingleHost) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	host := r.Host
-
-	println(r.Host)
-
-	if host == s.allowedHost {
-		s.handler.ServeHTTP(w, r)
-	} else {
-		println("I hate you!")
-		w.WriteHeader(403)
+		if host == allowedHost {
+			handler.ServeHTTP(w, r)
+		} else {
+			w.WriteHeader(403)
+		}
 	}
+
+	return http.HandlerFunc(ourFunc)
 }
 
 func myHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Success!"))
+	w.Write([]byte("Success!!!"))
 }
 
 func main() {
-	single := NewSingleHost(http.HandlerFunc(myHandler), "localhost:8080")
+	single := SingleHost(http.HandlerFunc(myHandler), "localhost:8080")
 
-	println("Listenting on port 8080")
+	println("Listening on port 8080")
 
 	http.ListenAndServe(":8080", single)
 }
